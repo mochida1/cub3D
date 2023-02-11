d999d9d979c8a0844430ceb727a84e51e544ee20
Henrique Mochida peixoto <hmochida@c1r8p1.42sp.org.br>
2023-02-11 15:07:55 -0300

    feat(textures): added function to destroy textures

--- src/main/main.c
+++ src/main/main.c
@@ -6,38 +6,13 @@
 /*   By: hmochida <hmochida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 20:02:44 by hmochida          #+#    #+#             */
-/*   Updated: 2023/02/11 14:56:54 by hmochida         ###   ########.fr       */
+/*   Updated: 2023/02/11 15:05:19 by hmochida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <mlx.h>
 #include "test.h"
-
-t_tx	*init_textures(t_mlx *mlx)
-{
-	t_tx	*tx;
-
-	tx = ft_calloc(4, sizeof(t_tx));
-	printf("north texture: %s\n", mlx->settings->north_texture);
-	tx[NORTH].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
-			mlx->settings->north_texture, &tx[NORTH].w, &tx[NORTH].h);
-	tx[SOUTH].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
-			mlx->settings->south_texture, &tx[SOUTH].w, &tx[SOUTH].h);
-	tx[WEST].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
-			mlx->settings->west_texture, &tx[WEST].w, &tx[WEST].h);
-	tx[EAST].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
-			mlx->settings->east_texture, &tx[EAST].w, &tx[EAST].h);
-	tx[NORTH].data = mlx_get_data_addr(tx[NORTH].tx_img_ptr, &tx[NORTH].bpp,
-			&tx[NORTH].sz_line, &tx[NORTH].endian);
-	tx[SOUTH].data = mlx_get_data_addr(tx[SOUTH].tx_img_ptr, &tx[SOUTH].bpp,
-			&tx[SOUTH].sz_line, &tx[SOUTH].endian);
-	tx[WEST].data = mlx_get_data_addr(tx[WEST].tx_img_ptr, &tx[WEST].bpp,
-			&tx[WEST].sz_line, &tx[WEST].endian);
-	tx[EAST].data = mlx_get_data_addr(tx[EAST].tx_img_ptr, &tx[EAST].bpp,
-			&tx[EAST].sz_line, &tx[EAST].endian);
-	return (tx);
-}
 
 void	init_images(t_mlx *mlx, t_raw_map *map)
 {
@@ -58,7 +33,6 @@ int	main(int argc, char *argv[])
 	t_raw_map	*map;
 	t_mlx		*mlx;
 	t_settings	*settings;
-	int			y;
 
 	input_parsing(argc, argv);
 	map = init_map("map.cub");
@@ -69,15 +43,10 @@ int	main(int argc, char *argv[])
 	settings = ft_calloc(1, sizeof(t_settings));
 	get_settings(settings, map->raw_cfg);
 	mlx->settings = settings;
-	y = 0;
-	while (mlx->map->raw_layout[y] != 0)
-	{
-		printf("%s", mlx->map->raw_layout[y]);
-		y++;
-	}
 	init_images(mlx, map);
 	event_handler(mlx);
 	render_images(mlx);
 	mlx_loop(mlx->mlx_ptr);
 	map = destroy_map(map);
+	mlx->tx = destroy_tx(mlx);
 }
