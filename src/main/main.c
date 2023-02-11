3defce638c8423d2ed4504e3e7b5c230f8d7e404
Henrique Mochida peixoto <hmochida@c1r8p1.42sp.org.br>
2023-02-11 14:57:43 -0300

    feat(textures): now loaded. Must destroy at exit

--- src/main/main.c
+++ src/main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: viferrei <viferrei@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: hmochida <hmochida@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 20:02:44 by hmochida          #+#    #+#             */
-/*   Updated: 2023/02/07 19:36:35 by viferrei         ###   ########.fr       */
+/*   Updated: 2023/02/11 14:56:54 by hmochida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,34 @@
 #include <mlx.h>
 #include "test.h"
 
+t_tx	*init_textures(t_mlx *mlx)
+{
+	t_tx	*tx;
+
+	tx = ft_calloc(4, sizeof(t_tx));
+	printf("north texture: %s\n", mlx->settings->north_texture);
+	tx[NORTH].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
+			mlx->settings->north_texture, &tx[NORTH].w, &tx[NORTH].h);
+	tx[SOUTH].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
+			mlx->settings->south_texture, &tx[SOUTH].w, &tx[SOUTH].h);
+	tx[WEST].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
+			mlx->settings->west_texture, &tx[WEST].w, &tx[WEST].h);
+	tx[EAST].tx_img_ptr = mlx_xpm_file_to_image(mlx->mlx_ptr,
+			mlx->settings->east_texture, &tx[EAST].w, &tx[EAST].h);
+	tx[NORTH].data = mlx_get_data_addr(tx[NORTH].tx_img_ptr, &tx[NORTH].bpp,
+			&tx[NORTH].sz_line, &tx[NORTH].endian);
+	tx[SOUTH].data = mlx_get_data_addr(tx[SOUTH].tx_img_ptr, &tx[SOUTH].bpp,
+			&tx[SOUTH].sz_line, &tx[SOUTH].endian);
+	tx[WEST].data = mlx_get_data_addr(tx[WEST].tx_img_ptr, &tx[WEST].bpp,
+			&tx[WEST].sz_line, &tx[WEST].endian);
+	tx[EAST].data = mlx_get_data_addr(tx[EAST].tx_img_ptr, &tx[EAST].bpp,
+			&tx[EAST].sz_line, &tx[EAST].endian);
+	return (tx);
+}
+
 void	init_images(t_mlx *mlx, t_raw_map *map)
 {
+	mlx->tx = init_textures(mlx);
 	ft_mlx_init_image(mlx, WINDOW_W, WINDOW_H, "cube");
 	ft_mlx_init_image(mlx, (map->raw_map_max_len - 1) * 2, \
 		map->raw_layout_size * 2, "minimap");
